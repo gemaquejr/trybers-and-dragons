7bea5a8d79a0072894e3cfdef48b15f84d0e8bd5
gemaquejr <gemaquejr@hotmail.com>
2023-03-22 08:54:23 -0300

    Refatora Dwarf.ts

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,20 +2,20 @@ import Race from './Race';
 
 class Dwarf extends Race {
   private _maxLifePoints: number;
-  private static _createdRacesInstances = 0;
+  static numinstacia = 0;
   
   constructor(name: string, dexterity: number) {
     super(name, dexterity);  
     this._maxLifePoints = 80;
-    Dwarf._createdRacesInstances += 1;
-  }
-   
-  static createdRacesInstances(): number {
-    return Dwarf._createdRacesInstances;
+    Dwarf.numinstacia += 1;
   }
   
   public get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+   
+  static createdRacesInstances(): number {
+    return Dwarf.numinstacia;
   }
 }
   
