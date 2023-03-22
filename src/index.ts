242715a3aaa333203afbafba8b69b3da26d2b60a
gemaquejr <gemaquejr@hotmail.com>
2023-03-22 09:45:20 -0300

    Adiciona index.ts em src

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import Character from './Character';
+import Monster from './Monster';
+import Dragon from './Dragon';
+import PVP from './Battle/PVP';
+import PVE from './Battle/PVE';
+import Battle from './Battle/Battle';
+
+const player1 = new Character('airam');
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+const player2 = new Character('aira');
+const player3 = new Character('antonio');
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+export const runBattles = (battles: Battle[]) => {
+  battles.forEach((e) => {
+    e.fight();
+  });
+};
+
+export { player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+};
