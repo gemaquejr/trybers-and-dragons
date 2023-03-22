66b8ae5faa907f51b139cf7edc0a2cb30057223a
gemaquejr <gemaquejr@hotmail.com>
2023-03-22 08:40:44 -0300

    Refatora Warrior.ts

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -3,20 +3,20 @@ import { EnergyType } from '../Energy';
 
 class Warrior extends Archetype {
   private _energyType: EnergyType;
-  private static _createdArchetypeInstances = 0;
+  static numinstacia = 0;
   
   constructor(name: string) {
     super(name);  
     this._energyType = 'stamina';
-    Warrior._createdArchetypeInstances += 1;
-  }
-   
-  static createdArchetypeInstances(): number {
-    return Warrior._createdArchetypeInstances;
+    Warrior.numinstacia += 1;
   }
   
   public get energyType(): EnergyType {
     return this._energyType;
+  }
+   
+  static createdArchetypeInstances(): number {
+    return Warrior.numinstacia;
   }
 }
   
